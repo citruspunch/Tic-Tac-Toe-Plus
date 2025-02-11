6751faf10848a4d9955640173b54bb0ff268f307
Andrés Tobar <152671606+citruspunch@users.noreply.github.com>
2025-02-10 21:53:55 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function Square({ value, onSquareClick, isHighlighted }) {
+  return (
+    <button
+      className={`square btn btn-light ${isHighlighted ? "highlight" : ""}`}
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay, lastMove }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    onPlay(nextSquares, i);
+  }
+
+  const winner = calculateWinner(squares);
+  let status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
+
+  return (
+    <div className="container text-center">
+      <div className="status mb-3 h4">{status}</div>
+      <div className="board">
+        {squares.map((square, i) => (
+          <Square
+            key={i}
+            value={square}
+            onSquareClick={() => handleClick(i)}
+            isHighlighted={lastMove === i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const [lastMove, setLastMove] = useState(null);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares, moveIndex) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setLastMove(moveIndex);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setLastMove(null);
+  }
+
+  return (
+    <div className="game container d-flex flex-column align-items-center mt-5">
+      <Board
+        xIsNext={xIsNext}
+        squares={currentSquares}
+        onPlay={handlePlay}
+        lastMove={lastMove}
+      />
+      <div className="game-info mt-3">
+        <ol className="list-unstyled">
+          {history.map((_, move) => (
+            <li key={move}>
+              <button
+                className="btn btn-primary btn-sm m-1"
+                onClick={() => jumpTo(move)}
+              >
+                {move ? `Go to move #${move}` : "Go to game start"}
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
